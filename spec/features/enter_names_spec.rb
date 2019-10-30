ca5bf1574c275657fd7b5601da1ffe6f8c8fb7a5
Topher <topherb99@gmail.com>
2019-10-30 16:17:19 +0000

    DRY up spec file

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,16 +1,12 @@
 feature 'Enter names' do
 
-  before do
-    visit('/')
-    fill_in :player_1_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
-  end
   scenario 'submitting names' do
+    sign_in_and_play
     expect(page).to have_content 'Dave vs. Mittens'
   end
 
   scenario 'Player 2 HP' do
+    sign_in_and_play
     expect(page).to have_content 'Mittens: 100HP'
   end
 end
