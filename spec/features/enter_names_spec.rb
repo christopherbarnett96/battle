9d717faf957da3f1ebcc356eb5f423613d275341
Topher <topherb99@gmail.com>
2019-10-30 17:43:26 +0000

    Added attack function

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,12 +1,21 @@
 feature 'Enter names' do
+  context "log in" do
+    scenario 'submitting names' do
+      sign_in_and_play
+      expect(page).to have_content 'Dave vs. Mittens'
+    end
 
-  scenario 'submitting names' do
-    sign_in_and_play
-    expect(page).to have_content 'Dave vs. Mittens'
+    scenario 'Player 2 HP' do
+      sign_in_and_play
+      expect(page).to have_content 'Mittens: 100HP'
+    end
   end
 
-  scenario 'Player 2 HP' do
+context 'attack' do
+  scenario 'confirmation of attack' do
     sign_in_and_play
-    expect(page).to have_content 'Mittens: 100HP'
+    click_button "Player 1 Attack!"
+    expect(page).to have_content 'Dave attacked Mittens'
   end
+end
 end
