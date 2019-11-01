e6dd1e6009230548e6e35b13abbbc93ff7594fa1
Topher <topherb99@gmail.com>
2019-11-01 11:38:13 +0000

    Added switching turns functionality to the model

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,17 @@
 class Game
 
 
-  attr_reader :player_1, :player_2
-  
+  attr_reader :player_1, :player_2, :current_turn
+
   def initialize(name_1, name_2)
     @player_1 = Player.new(name_1)
     @player_2 = Player.new(name_2)
+    @current_turn = @player_1.name
   end
 
   def attack(player)
     player.receive_damage
+    @current_turn = player.name
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,10 +2,16 @@ require 'game'
 
 describe Game do
   subject(:game) { described_class.new('Dave', 'Mittens') }
-  let(:dave) { double :player }
-  let(:mittens) { double :player }
+  let(:dave) { double :player, name: 'Dave' }
+  let(:mittens) { double :player, name: 'Mittens' }
+
+  before do (:each)
+    allow(mittens).to receive(:receive_damage)
+    allow(dave).to receive(:receive_damage)
+  end
 
   describe '#attack' do
+
     it 'damages the player' do
       expect(mittens).to receive(:receive_damage)
       game.attack(mittens)
@@ -13,12 +19,27 @@ describe Game do
   end
 
   context '#start game' do
+
     it 'starts a game with a 1st player' do
       expect(game.player_1.name).to eq 'Dave'
     end
 
     it 'starts a game with a 2nd player' do
       expect(game.player_2.name).to eq 'Mittens'
+    end
+  end
+
+  context '#turns' do
+
+    it 'switches turn after first turn' do
+      game.attack(mittens)
+      expect(game.current_turn).to eq 'Mittens'
+    end
+
+    it 'second turn changes player' do
+      game.attack(mittens)
+      game.attack(dave)
+      expect(game.current_turn).to eq 'Dave'
     end
   end
 end
